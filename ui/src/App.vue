71d6e76ee0ad15224bf1031a22ee6947c8ba2689
Joseph Nagy <nagyjoseph9@gmail.com>
2021-07-14 16:44:27 -0400

    implemented routes

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1>Kate &amp; Ally's Restaurant </h1>
+  <div id="app"> 
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
+    <router-view />
+
+  </div>
 </template>
 
-<script>
 
-</script>
 
 <style>
 #app {
@@ -13,6 +19,18 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>

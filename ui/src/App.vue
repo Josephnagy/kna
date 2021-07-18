92c9b36aea38b0d4c5c2a8bb68cc9eb02417e7cb
Joseph Nagy <nagyjoseph9@gmail.com>
2021-07-18 19:26:37 -0400

    added location

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app"> 
     <h1> Kate &amp; Ally's Restaurant</h1>
+    <h2> Located in Forked River NJ</h2>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>

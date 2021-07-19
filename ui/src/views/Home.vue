8a04ec345ddd0508be55716c67ee6c3168126f02
Joseph Nagy <nagyjoseph9@gmail.com>
2021-07-19 19:33:42 -0400

    removed hello world component

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
+    
+    <!-- Image Carousel -->
+    <home-page-carousel> </home-page-carousel>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HomePageCarousel from '@/components/HomePageCarousel.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home', 
+  components: {
+    HomePageCarousel
+  }
 }
 </script>

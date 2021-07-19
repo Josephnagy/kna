1ce97322d797b1c611f48b5bdf0adcb437115894
Joseph Nagy <nagyjoseph9@gmail.com>
2021-07-19 19:33:17 -0400

    created carousel component

---
+++ ui/src/components/HomePageCarousel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1> Image Carousel</h1>
+      
+    <b-carousel
+        id="home-page-carousel"
+        :interval="4000"
+        controls
+        indicators
+        img-width="1024"
+        img-height="480"
+        style="text-shadow: 1px 1px 2px #333;"
+    >
+        <b-carousel-slide
+        caption="First slide"
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+      <b-carousel-slide
+        caption="Second slide"
+        img-src="https://picsum.photos/1024/480/?image=54"
+      ></b-carousel-slide>
+
+
+    </b-carousel>
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'HomePageCarousel'
+}
+</script> 
